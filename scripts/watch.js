0cc00d9d517eb55a28feb4491928116ed1defbd4
Koji AGAWA <atty303@gmail.com>
2019-05-08 22:31:05 +0900

    improved stability of serve

--- scripts/watch.js
+++ scripts/watch.js
@@ -7,7 +7,8 @@ async function runBundle() {
   const hostBundler = new Bundler(path.join(root, 'src/host/main.ts'), {
     watch: true,
     outFile: 'host',
-    hmr: false
+    hmr: false,
+    cacheDir: '.cache-host',
   })
   const browserBundler = new Bundler(path.join(root, 'src/browser/main.html'), {
     watch: true,
@@ -15,20 +16,26 @@ async function runBundle() {
     publicUrl: './',
     hmr: true,
     hmrHostname: 'localhost',
+    cacheDir: '.cache-browser',
   })
 
   hostBundler.on('bundled', (bundle) => {
     browserBundler.onChange(path.join(root, 'src/browser/hmr.ts'))
   })
 
+  if (false) {
+    await Promise.all([hostBundler.bundle(), browserBundler.bundle()])
+  } else {
+    hostBundler.on('bundled', () => browserBundler.bundle())
+    hostBundler.bundle()
   // ;[hostBundler, browserBundler].forEach(async (bundler) => {
   //   await new Promise((resolve) => {
   //     bundler.on('bundled', resolve)
   //     bundler.bundle()
   //   })
   // })
+  }
 
-  await Promise.all([hostBundler.bundle(), browserBundler.bundle()])
 }
 
 runBundle()
