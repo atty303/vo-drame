e148833d90a3eda67edda75d999ea85b80e10d35
Koji AGAWA <atty303@gmail.com>
2019-05-09 22:48:05 +0900

    divide App.vue into ScenarioView.vue

--- src/browser/App.vue
+++ src/browser/App.vue
@@ -1,47 +1,17 @@
 <template>
   <div :style="appStyle">
     <panel-menu v-model="selectedPanelMenu"></panel-menu>
-    <q-layout view="hHh Lpr lFf" v-if="selectedPanelMenu === 'scenario'">
-      <q-header>
-        <q-toolbar class="" style="min-height: 30px">
-          <span>シーケンス</span>
-          <sequence-select v-model="selectedSequenceId" class="q-mx-xs"></sequence-select>
-          <q-btn label="新規作成" icon="add" size="xs" flat dense
-            v-if="canAdd"
-            @click="onAdd"
-          ></q-btn>
-          <q-btn label="同期" icon="sync" size="xs" flat dense
-            v-if="!canAdd"
-            :loading="isSyncing"
-            :disable="!canSync"
-            @click="onSync"
-          ></q-btn>
-          <q-separator vertical inset dark class="q-mx-sm"></q-separator>
-          <q-space></q-space>
-          <q-btn label="再読み込み" icon="build" size="xs" flat dense @click="onRefresh"></q-btn>
-        </q-toolbar>
-      </q-header>
-      <q-page-container>
-        <q-page padding>
-          <script-table :scene="scene" @sceneChanged="onSceneChanged"></script-table>
-          <!--scene-table :scene="scene" @sceneChanged="onSceneChanged"></scene-table-->
-        </q-page>
-      </q-page-container>
-    </q-layout>
+    <scenario-view v-if="selectedPanelMenu === 'scenario'"></scenario-view>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue, Inject, Watch} from 'vue-property-decorator'
-import SceneTable from './components/SceneTable.vue'
-import ScriptTable from './components/ScriptTable.vue'
-import {Scene} from './domain'
-import * as service from './service'
-import {csi} from './cse'
 import {colors} from 'quasar'
-import SequenceSelect from './components/SequenceSelect.vue'
-import {Premiere} from '../shared'
+
 import {default as PanelMenu, MenuItem} from './components/PanelMenu.vue'
+import ScenarioView from './components/ScenarioView.vue'
+import {csi} from './cse'
 
 function uiColorToCss(c: any): string {
   return `rgba(${Math.floor(c.red)},${Math.floor(c.green)},${Math.floor(c.blue)},${c.alpha / 255})`
@@ -49,19 +19,11 @@ function uiColorToCss(c: any): string {
 
 @Component({
   components: {
-    ScriptTable,
-    SequenceSelect,
-    SceneTable,
     PanelMenu,
-  }
+    ScenarioView,
+  },
 })
 export default class App extends Vue {
-  @Inject('scenarioService')
-  private scenarioService!: service.ScenarioService
-
-  scene: Scene = Scene.empty()
-  isSyncing: boolean = false
-  selectedSequenceId: Premiere.SequenceId = ''
   selectedPanelMenu: MenuItem = 'scenario'
 
   get appStyle(): string {
@@ -70,64 +32,14 @@ export default class App extends Vue {
     return `font-family: '${skin.baseFontFamily}'; font-size: ${skin.baseFontSize}px; background-color: ${bgColor}`
   }
 
-  get canAdd(): boolean {
-    return this.scene.isEmpty
-  }
-
-  get canSync(): boolean {
-    return this.selectedSequenceId.length > 0 && this.scene.nonEmpty
-  }
-
   async created() {
+    this.setSkin()
+  }
+
+  setSkin() {
     const skin = csi.getHostEnvironment().appSkinInfo
     colors.setBrand('primary', uiColorToCss(skin.systemHighlightColor))
     colors.setBrand('secondary', uiColorToCss(skin.appBarBackgroundColor.color))
-
-    const id = localStorage.getItem('selectedSequenceId')
-    if (id) this.selectedSequenceId = id
-  }
-
-  @Watch('selectedSequenceId')
-  async onSelectedSequenceIdChanged(): Promise<void> {
-    localStorage.setItem('selectedSequenceId', this.selectedSequenceId)
-
-    if (this.selectedSequenceId) {
-      this.scene = await this.scenarioService.loadScene(this.selectedSequenceId) || Scene.empty()
-    }
-  }
-
-  onRefresh(): void {
-    location.reload()
-  }
-
-  onSceneChanged(scene: Scene): void {
-    //console.log('onSceneChanged', scene)
-    this.scene = scene
-    if (this.selectedSequenceId) {
-      this.scenarioService.saveScene(this.selectedSequenceId, this.scene)
-    }
-  }
-
-  async onAdd(): Promise<void> {
-    this.onSceneChanged(Scene.newTemplate())
-  }
-
-  async onSync(): Promise<void> {
-    if (this.isSyncing || !this.selectedSequenceId) {
-      return
-    }
-    this.isSyncing = true
-    await this.scenarioService.syncScene(this.selectedSequenceId, this.scene)
-    this.isSyncing = false
   }
 }
 </script>
-
-<style lang="stylus">
-.q-layout__section--marginal {
-    background-color: var(--q-color-secondary) !important;
-    color: #fff;
-}
-</style>
-
-
---
+++ src/browser/components/ScenarioView.vue
@@ -0,0 +1,106 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="" style="min-height: 30px">
+        <span>シーケンス</span>
+        <sequence-select v-model="selectedSequenceId" class="q-mx-xs"></sequence-select>
+        <q-btn label="新規作成" icon="add" size="xs" flat dense
+          v-if="canAdd"
+          @click="onAdd"
+        ></q-btn>
+        <q-btn label="同期" icon="sync" size="xs" flat dense
+          v-if="!canAdd"
+          :loading="isSyncing"
+          :disable="!canSync"
+          @click="onSync"
+        ></q-btn>
+        <q-separator vertical inset dark class="q-mx-sm"></q-separator>
+        <q-space></q-space>
+        <q-btn label="再読み込み" icon="build" size="xs" flat dense @click="onRefresh"></q-btn>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <q-page padding>
+        <script-table :scene="scene" @sceneChanged="onSceneChanged"></script-table>
+        <!--scene-table :scene="scene" @sceneChanged="onSceneChanged"></scene-table-->
+      </q-page>
+    </q-page-container>
+  </q-layout>
+
+</template>
+
+<script lang="ts">
+import {Component, Vue, Inject, Watch} from 'vue-property-decorator'
+
+import SceneTable from './SceneTable.vue'
+import ScriptTable from './ScriptTable.vue'
+import SequenceSelect from './SequenceSelect.vue'
+
+import {Scene} from '../domain'
+import * as service from '../service'
+import {csi} from '../cse'
+import {Premiere} from '../../shared'
+
+@Component({
+  components: {
+    ScriptTable,
+    SequenceSelect,
+    SceneTable,
+  },
+})
+export default class ScenarioView extends Vue {
+  @Inject('scenarioService')
+  private scenarioService!: service.ScenarioService
+
+  scene: Scene = Scene.empty()
+  isSyncing: boolean = false
+  selectedSequenceId: Premiere.SequenceId = ''
+
+  get canAdd(): boolean {
+    return this.scene.isEmpty
+  }
+
+  get canSync(): boolean {
+    return this.selectedSequenceId.length > 0 && this.scene.nonEmpty
+  }
+
+  async created() {
+    const id = localStorage.getItem('selectedSequenceId')
+    if (id) this.selectedSequenceId = id
+  }
+
+  @Watch('selectedSequenceId')
+  async onSelectedSequenceIdChanged(): Promise<void> {
+    localStorage.setItem('selectedSequenceId', this.selectedSequenceId)
+
+    if (this.selectedSequenceId) {
+      this.scene = await this.scenarioService.loadScene(this.selectedSequenceId) || Scene.empty()
+    }
+  }
+
+  onRefresh(): void {
+    location.reload()
+  }
+
+  onSceneChanged(scene: Scene): void {
+    //console.log('onSceneChanged', scene)
+    this.scene = scene
+    if (this.selectedSequenceId) {
+      this.scenarioService.saveScene(this.selectedSequenceId, this.scene)
+    }
+  }
+
+  async onAdd(): Promise<void> {
+    this.onSceneChanged(Scene.newTemplate())
+  }
+
+  async onSync(): Promise<void> {
+    if (this.isSyncing || !this.selectedSequenceId) {
+      return
+    }
+    this.isSyncing = true
+    await this.scenarioService.syncScene(this.selectedSequenceId, this.scene)
+    this.isSyncing = false
+  }
+}
+</script>
